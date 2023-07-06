b399565e86a724ac9426fe35e3d2e32de6f5474a
ArshyaVfx <102918159+ArshyaVfx@users.noreply.github.com>
2023-07-06 23:51:25 +0330

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+-- Leaked By: baba vf
+
+server_scripts {
+	'config.lua',
+	'server/server.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/client.lua'
+}
